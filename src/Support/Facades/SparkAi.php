c632ecbad0328ec9bb0d4f363788622d185a931f
jiannei <longjian.huang@foxmail.com>
2023-11-25 18:49:47 +0800

    fix: facade

--- src/Support/Facades/SparkAi.php
+++ src/Support/Facades/SparkAi.php
@@ -18,10 +18,9 @@ use Illuminate\Support\Facades\Facade;
  * @method static \Jiannei\SparkAi\SparkAi withContext(array $content)
  * @method static string getLatestResponse()
  * @method static string getLatestTokenUsage()
- * @method static string getAnswer()
  * @method static array history()
  *
- * @see \\Jiannei\SparkAi\SparkAi
+ * @see \Jiannei\SparkAi\SparkAi
  */
 class SparkAi extends Facade
 {
